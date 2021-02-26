09abfe9d80771b79ab1010b755c3a60cb862fc8b
greta-marie <79710575+greta-marie@users.noreply.github.com>
2021-02-26 11:15:28 -0500

    Add files via upload

---
+++ IdahoCanyons.java
@@ -0,0 +1,20 @@
+
+package destinations;
+
+/**
+ *Simulation class for a project management
+ *exercise
+ *@author Greta Mueller
+ */
+public class IdahoCanyons {
+    
+    public static void main(String[] args){
+        //calls println method to send text to the console
+        System.out.println("I am in class IdahoCanyons");
+        System.out.println("Upload photos of the canyons");
+        
+    }//close main
+    
+    
+    
+}//close class IdahoCanyons
